00edc0ed4331f70279834452d68c075fd3e0eee0
Jawid <jawidgulzai9@gmail.com>
2024-02-06 09:31:35 -0800

    This is my final project

---
+++ src/test/java/tek/bdd/runner/TestRunner.java
@@ -0,0 +1,21 @@
+package tek.bdd.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "classpath:feature",
+        glue = "tek.bdd.steps",
+        tags = "@Regression",
+        dryRun = false,
+        monochrome = true,
+        snippets = CucumberOptions.SnippetType.CAMELCASE,
+        plugin = {
+                "html:target/cucumber-report/report.html",
+                "json:target/jsonReports/jsonReport.json",
+                "pretty"
+        })
+public class TestRunner {
+}
